53905c5848881cf8dc1e3cf2d67260651f57fe59
oluku-project <olukukstu@gmail.com>
2023-03-24 22:02:03 +0000

    alt: settings on db

--- IKA_NET_HMS/settings.py
+++ IKA_NET_HMS/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = "IKA_NET_HMS.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "framada",
-#         "USER": "root",
-#         "HOST":"localhost",
-#         "PORT":"3306",
-#         "PASSWORD":"ymcmbysl22"
-#     }
-# }
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "framada",
+        "USER": "root",
+        "HOST":"localhost",
+        "PORT":"3306",
+        "PASSWORD":"ymcmbysl22"
+    }
 }
-}
+# DATABASES = {
+# 'default': {
+#     'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
+# }
+# }
 
 
 # Password validation
